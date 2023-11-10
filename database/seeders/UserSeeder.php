03da8919ed00277050c797b2ff4694cc576b9419
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2023-11-10 09:54:59 +0700

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -19,7 +20,7 @@ class UserSeeder extends Seeder
                 'id_skpd' => '1',
                 'nama' => 'ekaf',
                 'username' => 'admin',
-                'password' => 'admins',
+                'password' => Hash::make('admins'),
             ],
         ];
 
